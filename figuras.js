3e964b5809bff0d7391cc06dcc3bffd857924de2
InkNico <ndjimenezj@icloud.com>
2021-12-06 10:23:27 -0500

    primeros console.log agregados

--- figuras.js
+++ figuras.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado);
+
+const pertimetroCuadrado = ladoCuadrado * 4;
+console.log ("El perímetro del cuadrado es: " + ladoCuadrado);
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log ("El perímetro del cuadrado es: " + areaCuadrado);
