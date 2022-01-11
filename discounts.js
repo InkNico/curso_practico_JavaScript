b2713021993b6d0610ae6b9ab7f2b94acb990a4e
InkNico <ndjimenezj@icloud.com>
2022-01-10 20:22:26 -0500

    discount .js and .html language update = en, javascript file finish, pending challange

--- discounts.js
+++ discounts.js
@@ -2,13 +2,26 @@
 const descuento = 18; */
 
 
-function calcularPrecioConDescuento(precio, descuento) {
-    const porcentajePrecioConDescuento = 100 - descuento;
-    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+function EstimateDiscountPrice(price, discount) {
+    const percentagePriceWithDiscount = 100 - discount;
+    const discountPrice = (price * percentagePriceWithDiscount) / 100;
 
-    return precioConDescuento;
+    return discountPrice;
 }
 
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const discountPrice = EstimateDiscountPrice(priceValue, discountValue);
+
+    const resultPrice = document.getElementById("Result-price");
+    resultPrice.innerText = "The final price after the discount is applied is: $" + discountPrice;
+}
 
 /* console.log({
     precioOriginal,
